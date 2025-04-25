b373c86f6e8c852d77be0389e7af6b634c5f7bfb
dahlaran <joseph.demerss@hotmail.fr>
2025-04-25 18:08:30 +0200

    Network security

--- app/src/main/java/com/dahlaran/newmovshow/presentation/favorite/MediaFavoriteItem.kt
+++ app/src/main/java/com/dahlaran/newmovshow/presentation/favorite/MediaFavoriteItem.kt
@@ -5,11 +5,8 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
