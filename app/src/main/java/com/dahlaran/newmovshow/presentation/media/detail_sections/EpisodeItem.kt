d0707cd017437b6775ed2d75cef3efc675427b64
dahlaran <joseph.demerss@hotmail.fr>
2025-04-27 13:24:42 +0200

    Rename LazyColumn into Column

--- app/src/main/java/com/dahlaran/newmovshow/presentation/media/detail_sections/EpisodeItem.kt
+++ app/src/main/java/com/dahlaran/newmovshow/presentation/media/detail_sections/EpisodeItem.kt
@@ -3,6 +3,7 @@ package com.dahlaran.newmovshow.presentation.media.detail_sections
 import androidx.compose.animation.animateContentSize
 import androidx.compose.animation.core.Spring
 import androidx.compose.animation.core.spring
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -17,9 +18,12 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.dahlaran.newmovshow.R
 import com.dahlaran.newmovshow.domain.model.Episode
 
 
@@ -44,7 +48,7 @@ fun EpisodeItem(
         ) {
             Text(
                 text = episode.title,
-                style = MaterialTheme.typography.titleSmall,
+                style = MaterialTheme.typography.titleMedium,
                 modifier = Modifier.padding(8.dp),
                 overflow = TextOverflow.Ellipsis
             )
@@ -52,16 +56,22 @@ fun EpisodeItem(
             if (isExpanded) {
                 Text(
                     text = episode.getTextSummary(),
-                    style = MaterialTheme.typography.titleMedium,
-                    modifier = Modifier.padding(8.dp).fillMaxWidth()
+                    style = MaterialTheme.typography.bodyMedium,
+                    modifier = Modifier
+                        .padding(8.dp)
+                        .fillMaxWidth()
                 )
             } else {
-                Button(
-                    onClick = { isExpanded = !isExpanded },
-                    modifier = Modifier.align(Alignment.CenterHorizontally)
-                ) {
-                    Text("Show Spoiler !")
-                }
+                Text(
+                    text = stringResource(R.string.show_summary),
+                    style = MaterialTheme.typography.bodyMedium,
+                    color = MaterialTheme.colorScheme.primary,
+                    textAlign = TextAlign.Center,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(vertical = 8.dp)
+                        .clickable { isExpanded = !isExpanded }
+                )
             }
         }
     }
